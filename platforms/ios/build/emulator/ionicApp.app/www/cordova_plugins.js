6d3a4bf93e0d4f4f90a381bcaa187da312a07a0c
Phruek <phruek1412@gmail.com>
2018-03-06 11:06:53 +0700

    add build folder

---
+++ platforms/ios/build/emulator/ionicApp.app/www/cordova_plugins.js
@@ -0,0 +1,56 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "cordova-plugin-device.device",
+    "file": "plugins/cordova-plugin-device/www/device.js",
+    "pluginId": "cordova-plugin-device",
+    "clobbers": [
+      "device"
+    ]
+  },
+  {
+    "id": "cordova-plugin-splashscreen.SplashScreen",
+    "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+    "pluginId": "cordova-plugin-splashscreen",
+    "clobbers": [
+      "navigator.splashscreen"
+    ]
+  },
+  {
+    "id": "cordova-plugin-ionic-webview.ios-wkwebview-exec",
+    "file": "plugins/cordova-plugin-ionic-webview/src/www/ios/ios-wkwebview-exec.js",
+    "pluginId": "cordova-plugin-ionic-webview",
+    "clobbers": [
+      "cordova.exec"
+    ]
+  },
+  {
+    "id": "cordova-plugin-ionic-keyboard.keyboard",
+    "file": "plugins/cordova-plugin-ionic-keyboard/www/ios/keyboard.js",
+    "pluginId": "cordova-plugin-ionic-keyboard",
+    "clobbers": [
+      "window.Keyboard"
+    ]
+  },
+  {
+    "id": "cordova-plugin-broadcaster.broadcaster",
+    "file": "plugins/cordova-plugin-broadcaster/www/broadcaster.js",
+    "pluginId": "cordova-plugin-broadcaster",
+    "clobbers": [
+      "broadcaster"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-whitelist": "1.3.3",
+  "cordova-plugin-device": "2.0.1",
+  "cordova-plugin-splashscreen": "5.0.2",
+  "cordova-plugin-ionic-webview": "1.1.16",
+  "cordova-plugin-ionic-keyboard": "2.0.5",
+  "cordova-plugin-add-swift-support": "1.7.1",
+  "cordova-plugin-broadcaster": "3.0.1"
+};
+// BOTTOM OF METADATA
+});
